0b4adb2179203072abb3ba6b4ac7e5bd7897c3d8
harshalghule1896 <harshalghule07@gmail.com>
2019-02-26 22:31:05 +0530

    Spotlight commit 1

---
+++ File Search/MainWindow.xaml.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace File_Search
+{
+    class FileException : Exception
+    {
+        public FileException(string exception)
+        {
+            MessageBox.Show(exception);
+        }
+    }
+
+    public partial class MainWindow : Window
+    {
+
+        Thread[] tobj;
+        DriveInfo[] allDrives;
+        ListBox listbox;
+      
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            tobj = null;
+            allDrives = DriveInfo.GetDrives();
+            tobj = new Thread[allDrives.Length];
+            listbox = new ListBox();
+            listbox.MouseDoubleClick += new MouseButtonEventHandler(ListboxMouseDoubleCilcked);
+            ResultGrid.Children.Add(listbox);
+
+        }
+
+        private void Listener(Object sender, RoutedEventArgs e)
+        {
+            String FileName = FileNameTextBox.Text;
+            FileNameTextBox.Text = null;
+            listbox.Items.Clear();
+
+            Button btn = (Button)sender;
+
+
+            switch (btn.Uid)
+            {
+                case "search":
+
+                    if (string.IsNullOrEmpty(FileName))
+                    {
+                        MessageBox.Show("Please Enter FileName !");
+                        return;
+                    }
+
+                    int i = 0;
+
+                    foreach (DriveInfo drive in allDrives)
+                    {
+                        if (drive.DriveType.ToString().Equals("Fixed"))
+                        { 
+                            tobj[i] = new Thread( new ThreadStart(() => { getfiles(drive.ToString(), FileName + "*"); }));
+                            tobj[i].Start();
+                            i++;
+                        }
+
+                    }
+
+                    break;
+
+                case "reset":
+                    FileNameTextBox.Text = null;
+                    listbox.Items.Clear();
+                    break;
+            }
+
+
+
+        }
+
+      /*  public static IEnumerable<String> GetFiles(String path, String pattern)
+        {
+
+            return Directory.EnumerateFiles(path, pattern).Union(Directory.EnumerateDirectories(path).SelectMany(a =>
+            {
+                try
+                {
+                    return GetFiles(a, pattern);
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    return Enumerable.Empty<String>();
+                }
+            }
+            ));
+
+
+        }
+        */
+
+        public void getfiles(string path, string pattern)
+        {
+            string[] dirs = null;
+            string[] files = null;
+
+            try
+            {
+
+                dirs = Directory.GetDirectories(path);
+
+                foreach (string d in dirs)
+                {
+                    getfiles(d, pattern);
+                }
+
+                files = Directory.GetFiles(path, pattern);
+
+                Application.Current.Dispatcher.Invoke(new Action(() =>
+                {
+                    foreach (string f in files)
+                    {
+                        listbox.Items.Add(f);
+                    }
+                }));
+            }
+            catch (UnauthorizedAccessException)
+            {
+
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+
+
+  
+        private void ListboxMouseDoubleCilcked(object sender, MouseButtonEventArgs e)
+        {
+
+            ListBox lb = (ListBox)sender;
+
+            if (File.Exists((String)lb.SelectedItem))
+            {
+
+                Process myProcess = new Process();
+
+                ProcessStartInfo myProcessStartInfo = new ProcessStartInfo(lb.SelectedItem.ToString());
+
+                myProcess.StartInfo = myProcessStartInfo;
+
+                myProcess.Start();
+            }
+
+
+
+        }
+
+    }
+}
---
+++ File Search/Search.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Windows;
+
+namespace File_Search
+{
+    class Search
+    {
+        static Mutex mutex;
+
+        public Search()
+        {
+            mutex = new Mutex();
+        }
+  
+        public static IEnumerable<String> GetFiles(String path, String pattern)
+        {
+
+            return Directory.EnumerateFiles(path, pattern).Union(Directory.EnumerateDirectories(path).SelectMany(a =>
+            {
+
+                try
+                {
+                    return GetFiles(a, pattern);
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    return Enumerable.Empty<String>();
+                }
+            }
+            ));
+
+
+        }
+
+        public static void DisplayText(IEnumerable<String> Enum)
+        {
+
+            try
+            {
+                mutex.WaitOne();
+
+                foreach (String s in Enum)
+                {
+                    MessageBox.Show(s);
+                }
+
+                mutex.ReleaseMutex();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+
+       }
+    }
+}
